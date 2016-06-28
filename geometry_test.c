6cfa04814d4a8a8a17e8b3c7a8e6e973232ee5c5
booksforever <kathryn.gray@colorado.edu>
2016-06-28 12:04:15 -0600

    Added area of triangle

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,45 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/*coord_2d_area_triange Test*/
+START_TEST(test_area_triangle)
+{
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
+	a.x = b.x = c.x = 0;
+	a.y = b.y = c.y = 0;
+	
+	double actual1 = coord_2d_area_triangle(&a, &b, &c);
+	ck_assert(actual1==(0.0));
+
+
+	a.x = 0.0;
+	b.x = 0.0;
+	c.x = 4.0;
+	a.y = 0.0;
+	b.y = 5.0;
+	c.y = 5.0;
+	
+	double actual2 = coord_2d_area_triangle(&a, &b, &c);
+	ck_assert(actual2==(10.0));
+	
+	
+	a.x = -1;
+	a.y = 0;
+	b.x = 21;
+	b.y = -1;
+	c.x = 20;
+	c.y = 30;
+	
+	double actual3 = coord_2d_area_triangle(&a, &b, &c);
+	ck_assert(actual3=(340.5));
+	
+	
+}
+END_TEST
+
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -161,12 +200,16 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
+    
+    TCase* tc_area_triangle=tcase_create("coord_2d_area_triangle");
+	tcase_add_test(tc_area_triangle, test_area_triangle);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_area_triangle);
+    
     /* Return Suite */
     return s;
 
